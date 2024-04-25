f480acbfd75ad38f9ba72e9f16699ac834b9cf7f
Deepak <yadavdeepak5035@gmail.com>
2024-04-25 18:25:57 +0530

    commit 2

--- AdminServer/src/main/java/com/alt/AdminServerApplication.java
+++ AdminServer/src/main/java/com/alt/AdminServerApplication.java
@@ -11,6 +11,8 @@ public class AdminServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdminServerApplication.class, args);
+		System.out.println();
+		System.out.println();
 	}
 
 }
